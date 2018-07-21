2feedae0588c2885eecbb696bebf1f0144838613
Carlos Azuaje <carlosjazzc1@gmail.com>
2018-07-21 13:55:11 -0400

    fix(deploy): create custom razzle config modify
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>

--- razzle.config.js
+++ razzle.config.js
@@ -1,3 +1,28 @@
+const isHeroku = require("is-heroku")
+const entries = require("object.entries")
+
 module.exports = {
-  modify: require("razzle-heroku"),
+  modify: (config, { target, dev }, webpack) => {
+    if (target !== "node") return config
+
+    const isDefinePlugin = plugin => plugin.constructor.name === "DefinePlugin"
+    const indexDefinePlugin = config.plugins.findIndex(isDefinePlugin)
+
+    if (indexDefinePlugin < 0) {
+      console.warn("Couldn't setup razzle-heroku, no DefinePlugin...")
+      return config
+    }
+
+    const { definitions } = config.plugins[indexDefinePlugin]
+    const newDefs = Object.assign({}, definitions);
+
+    if (isHeroku) {
+      delete newDefs["process.env.PORT"]
+      newDefs["process.env.RAZZLE_PUBLIC_DIR"] = '"/app/build/public"'
+    }
+
+    config.plugins[indexDefinePlugin] = new webpack.DefinePlugin(newDefs)
+
+    return config
+  }
 }
