fddc4916bd3ee3bbce39e323f608131270a20a63
Unknown <sehe89@gmail.com>
2019-03-10 17:47:18 +0100

    add custom marker

--- src/components/WasteBinInfo/index.js
+++ src/components/WasteBinInfo/index.js
@@ -1,18 +1,20 @@
 import React, {PureComponent} from 'react';
+import './styles.css'
 
 export default class WasteBinInfo extends PureComponent {
 
   render() {
+    
     const {info} = this.props;
     const displayName = `${info.type}`;
     const position = `(${info.latitude}, ${info.longitude})`
 
     return (
       <div>
-        <h3>
+        <h3 className="InfoTitle">
           {displayName}
         </h3>
-        <p>{position}</p>
+        <p className="InfoPosition">Location: {position}</p>
       </div>
     );
   }
--- src/components/WasteBinPin/index.js
+++ src/components/WasteBinPin/index.js
@@ -1,8 +1,7 @@
 import React, {PureComponent} from 'react';
+import './styles.css'
+import trash_can_img from '../../assets/images/double_trash_can.png'
 
-const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
-  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
-  C20.1,15.8,20.2,15.8,20.2,15.7z`;
 
 const pinStyle = {
   cursor: 'pointer',
@@ -16,14 +15,14 @@ export default class WasteBinPin extends PureComponent {
     const {size = 20, onClick} = this.props;
 
     return (
-      <svg
-        height={size}
-        viewBox="0 0 24 24"
-        style={{...pinStyle, transform: `translate(${-size / 2}px,${-size}px)`}}
+      <img
+        src={trash_can_img}
+        height="48px"
+        viewBox="0 0 100 100"
+        style={{...pinStyle}}
         onClick={onClick}
       >
-        <path d={ICON}/>
-      </svg>
+      </img>
     );
   }
 }
