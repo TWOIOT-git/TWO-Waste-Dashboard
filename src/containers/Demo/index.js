d41e6e1ed7e7e96aec29452ce4dc1164e529f7b3
Carlos Azuaje <carlosjazzc1@gmail.com>
2019-03-25 23:29:49 -0300

    fix conference levels
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>

--- src/containers/Demo/index.js
+++ src/containers/Demo/index.js
@@ -30,7 +30,7 @@ class Demo extends Component {
         .then(res => {
           this.setState({
             data: {
-              value: -1 * ((res.Items[0].bin_level / 200) * 100 - 100),
+              value: -1 * (((860 - res.Items[0].bin_level ) * 100) / 860 - 200 )  - 100,
               text: "Sensor ID40"
             }
           });
