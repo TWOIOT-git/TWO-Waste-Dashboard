7e6a14246eb52447021f1bc6dcd9c03d52dc1f79
Carlos Azuaje <carlosjazzc1@gmail.com>
2019-04-19 11:52:10 -0300

    login ui add favicon
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>

--- components/Head.js
+++ components/Head.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import NextHead from "next/head";
 
 const TITLE = "lidbot";
@@ -6,20 +7,33 @@ const DESCRIPTION = "lidbot Waste Dashboard";
 const OGURL = "https://razzle-client.herokuapp.com";
 const OGIMAGE = "https://razzle-client.herokuapp.com";
 
-export default (props) => (
+const Head = ({ title, description, ogImage, url }) => (
   <NextHead>
-    <title>{props.title || TITLE}</title>
-    <meta name="description" content={props.description || DESCRIPTION} />
+    <title>{title}</title>
+    <meta name="description" content={description} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta property="og:url" content={props.url || OGURL} />
-    <meta property="og:title" content={props.title || TITLE} />
-    <meta
-      property="og:description"
-      content={props.description || DESCRIPTION}
-    />
-    <meta name="twitter:site" content={props.url || OGURL} />
+    <meta property="og:url" content={url} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta name="twitter:site" content={url} />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || OGIMAGE} />
-    <meta property="og:image" content={props.ogImage || OGIMAGE} />
+    <meta name="twitter:image" content={ogImage} />
+    <meta property="og:image" content={ogImage} />
   </NextHead>
 );
+
+Head.defaultProps = {
+  title: TITLE,
+  description: DESCRIPTION,
+  url: OGURL,
+  ogImage: OGIMAGE
+};
+
+Head.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  url: PropTypes.string,
+  ogImage: PropTypes.string
+};
+
+export default Head;
