76004a4620f3963bd6b7b2dbdf33a62d89d45328
Andrzej Miller <andrzej@haircutinspiration.com>
2019-08-14 14:57:10 +0700

    revert changes in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,9 @@ const webpack = require('webpack')
 
 module.exports = {
   webpack: (config) => {
-    if(process.env.NODE_ENV !== "production") {
-      config.plugins.push(
-        new webpack.EnvironmentPlugin(process.env)
-      )
-    }
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(process.env)
+    )
     return config
   }
 }
