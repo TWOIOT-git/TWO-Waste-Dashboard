5526eb2566a82e5410ca7137c3fa3cb2fcc74a66
Andrzej Miller <andrzej@haircutinspiration.com>
2019-08-23 11:44:10 +0800

    redirect to https

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require("http");
+const https = require('https');
 const next = require("next");
+const fs = require('fs')
 const express = require("express");
 const compression = require("compression");
 
@@ -11,18 +13,53 @@ const handle = app.getRequestHandler();
 // Start the server
 app.prepare().then(() => {
   const server = express();
+
   server.disable("x-powered-by");
   server.use(compression());
+
+  // remove trailing slash
   server.get(/(.+)\/$/, (req, res) =>
-  res.redirect(301, req.url.replace(/\/$/, ""))
+    res.redirect(301, req.url.replace(/\/$/, ""))
   );
+
   server.get('/sensor/:id', (req, res) => {
     console.log('calling route /sensor/:id')
     return app.render(req, res, '/sensor', { id: req.params.id })
   })
+
+  // redirect http => https on production only
+  if(process.env.NODE_ENV === 'production') {
+    server.use((req, res, next) => {
+      console.log('secure: ' + req.secure);
+      console.log('proto: ' + req.header('x-forwarded-proto'));
+      if (req.header('x-forwarded-proto') !== 'https' && req.secure !== true) {
+        console.log('redirect');
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      } else {
+        next()
+      }
+    })
+  }
+
   server.get("*", handle);
-  const httpServer = http.createServer(server);
-  httpServer.listen(port, err => {
+
+  http.createServer(server)
+    .listen(port, err => {
     if (err) throw err;
+    else {
+      console.log(`[HTTP] Listening on port ${port}!`)
+    }
   });
+
+  // https.createServer({
+  //   key: fs.readFileSync('certs/server.key'),
+  //   cert: fs.readFileSync('certs/server.cert')
+  // }, server)
+  //   .listen(3001, err => {
+  //   if (err) throw err;
+  //   else {
+  //     console.log(`[HTTPS] Listening on port ${port}!`)
+  //   }
+  // });
+
 });
